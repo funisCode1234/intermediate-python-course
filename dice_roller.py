b75f71aadbfed5c731a4786f955f1210d3da6e2f
john lee <27mac@John-Lees-iMac.local>
2020-11-27 16:02:32 -0800

    Tutorial Complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,19 @@
 import random
 
 def main():
-  #print('You rolled a die')
-  roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+    dice_rolls = int(input('How many dice would you like to roll? '))
+    dice_size = int(input('How many sides are the dice? '))
+    dice_sum = 0
+    for i in range(0, dice_rolls): # runs the code twice
+        roll = random.randint(1, dice_size) # randomly picks a number from 1 through 6
+        dice_sum += roll # adds the dice rolls dice_sum = dice_sum + roll
+        if roll == 1:
+            print(f'You rolled a {roll}! Critical Fail')
+        elif roll == 6:
+            print(f'You rolled a {roll}! Critical Success!')
+        else:
+            print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
